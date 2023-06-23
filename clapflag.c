5302a544157156ee889883c10e2cdd171c936f44
Vsevolod Nitsenko <130823130+vsnits@users.noreply.github.com>
2023-06-23 17:43:09 +0300

    Update clapflag.c

--- clapflag.c
+++ clapflag.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-  
-// 1 125 899 906 842 597
-// 18 014 398 509 481 951
-// 72 057 594 037 927 931
-   
+     
 int ipow(unsigned long long int p, int q) {
     unsigned long long int m = p;
     for(int i = 1; i < q; i++) { p = p*m; }
@@ -16,7 +12,9 @@ unsigned long long int clapflag(int n) {
     unsigned long long int p = len*len - len;
     int *p1 = calloc(len, sizeof(int));
     int *p2 = calloc(len, sizeof(int));
-    if(p1 == NULL || p2 == NULL) { printf("Memmory allocation failed!\n"); return len; }
+    if(p1 == NULL || p2 == NULL) {
+      printf("Memmory allocation failed!\n"); return len;
+      }
     
     for(int i = 1; i < len; i++) { p1[i] = 1; p2[i] = 1; }
     
@@ -37,15 +35,10 @@ unsigned long long int clapflag(int n) {
         }
       printf("Nothing found? Was everithing right?\n");
       return len;
-    }
+    };
    
    int main() {
-    /*  FILE *res = fopen("Largeprime.txt", "w+");
-      if(res != NULL) { fprintf(res, clapflag(4)); } else { printf("File couldn't be opened!"); return 0; }
-    */
       printf("%llu\n", clapflag(30));
       return 1;
-     }
-   
-   
-
+     };
+  
