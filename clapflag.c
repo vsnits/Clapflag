dccecbdaee710bd0db02a9eb628df63e783326cf
Vsevolod Nitsenko <130823130+vsnits@users.noreply.github.com>
2023-06-25 12:00:29 +0300

    Update clapflag.c

--- clapflag.c
+++ clapflag.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 // 288 230 376 151 711 717 at depth 29
+// Recommended flag -O1
 
 #define setbit(A, k) (A[k/32] |= (1 << (k %32)))
 #define getbit(A, k) (A[k/32] & (1 << (k % 32)))
@@ -10,20 +11,20 @@
 unsigned long long int clapflag(int n) {
     unsigned long long int len = 1 << n;
     unsigned long long int p = len*len - len;
-    char *p1 = calloc(len, 1);
-    char *p2 = calloc(len, 1);
-    if(p1 == NULL || p2 == NULL) {
+    int *p1 = calloc(len, 1);
+    int *p2 = calloc(len, 1);
+    if(p1 == NULL || p2 == NULL) { 
         printf("Memmory allocation failed!\n"); return len; 
         }
     
     setbit(p1, 0); setbit(p2, 0); // Notice, this is important!
-    
-    for(int i = 2; i < len; i++) {
+   
+    for(unsigned long long int i = 2; i < len; i++) {
         if(!getbit(p1, i)) {
-            for(int e = 2*i; e < len; e += i) {
+            for(unsigned long int e = 2*i; e < len; e += i) {
                 setbit(p1, e); continue;
                 }
-            for(int e = i - (p % i); e < len; e += i) {
+            for(unsigned long long int e = i - (p % i); e < len; e += i) {
                 setbit(p2, e); continue;
                 }
             }; continue;
@@ -32,12 +33,13 @@ unsigned long long int clapflag(int n) {
     for(unsigned long long int i = len-1; i > 0; i--) {
         if(!getbit(p2, i)) { free(p1); free(p2); return i+p; }
         }
+    free(p1); free(p2); // Don't forget keep clean
     printf("Nothing found? Was everithing right?\n");
     return len;
     };
 
 int main() {
-    printf("%llu\n", clapflag(29));
+    printf("%llu\n", clapflag(28));
     return 1;
     };
 
